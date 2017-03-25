1e8637d382d9cc711ea12e18b0c314991eee2e9b
Mrunal Dave <davemrunal93@gmail.com>
2017-03-25 17:50:48 -0400

    Edit variable names

--- src/pagereplacement/PageReplacement.java
+++ src/pagereplacement/PageReplacement.java
@@ -28,17 +28,17 @@ public class PageReplacement {
         String path = "D:\\JAVA Projects\\PageReplacement\\src\\pagereplacement\\";
         path = path + filename + ".txt";
         int pages = 0; //Number of pages occupied by the process in memory
-        ArrayList<Integer> pr = new ArrayList<Integer>(); //Stores Page references
-        HashMap<Integer, Integer> rs = new HashMap<Integer, Integer>(); //Stores resident set. key is page number and value denotes used bit.
+        ArrayList<Integer> pageReferences = new ArrayList<Integer>(); //Stores Page references
+        HashMap<Integer, Integer> residentSet = new HashMap<Integer, Integer>(); //Stores resident set. key is page number and value denotes used bit.
         int time = 0;
         int F = 2;
         /*If F is small, principle of locality would not be much beneficial hence greater number of page faults. 
         If F is large, locality benefits kick in and fewer page faults. 
         */
         
-        rs.put(1,0);
-        rs.put(4,0);
-        rs.put(5,0);    
+        residentSet.put(1,0);
+        residentSet.put(4,0);
+        residentSet.put(5,0);    
         int fault1 = 0;
         try {
             File file = new File(path);
@@ -47,31 +47,31 @@ public class PageReplacement {
             //StringBuffer stringBuffer = new StringBuffer();
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                pr.add(Integer.parseInt(line));
+                pageReferences.add(Integer.parseInt(line));
             }
             fileReader.close();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-        pages = pr.get(0);
-        pr.remove(0);
+        pages = pageReferences.get(0);
+        pageReferences.remove(0);
         
         //PFF Implementation
-        for (int i = 0; i < pr.size(); i++) {
-            int x = pr.get(i);
-            boolean bln = rs.containsKey(x);
+        for (int i = 0; i < pageReferences.size(); i++) {
+            int x = pageReferences.get(i);
+            boolean bln = residentSet.containsKey(x);
             if (bln == false) {
                 fault1++;
                 if (time < F) {
-                    rs.put(x, 1);
+                    residentSet.put(x, 1);
                 } else {
-                    rs.values().removeAll(Collections.singleton(0));
-                    Set<Integer> keys = rs.keySet();
+                    residentSet.values().removeAll(Collections.singleton(0));
+                    Set<Integer> keys = residentSet.keySet();
                     for (Integer key : keys) {
-                        rs.put(key, 0);
+                        residentSet.put(key, 0);
                     }
-                    rs.put(x, 1);
+                    residentSet.put(x, 1);
                 }
                 time=0;
             } else {
